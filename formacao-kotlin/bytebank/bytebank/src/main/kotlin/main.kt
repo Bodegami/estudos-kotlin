4b033733e5240388acca41b890afe179dac85fd6
Renato <rena.f.oliveira@gmail.com>
2022-12-22 22:59:53 -0300

    sobre os tipos de variaveis

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+
+// Use 'var' para variaveis que podem mudar o valor
+// use 'val' para variaveis imutaveis
+
 fun main() {
-    print("Bem vindo ao Bytebank!")
+    println("Bem vindo ao Bytebank!")
+    val titular = "Renato"
+
+    //String template
+    println("titular: $titular")
+
 }
