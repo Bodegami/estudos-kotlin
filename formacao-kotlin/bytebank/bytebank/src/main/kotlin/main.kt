57bea79d7a0e19f943f6b7aa701d417808ff8f73
Renato <rena.f.oliveira@gmail.com>
2022-12-22 23:08:59 -0300

    restrições de variaveis

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
@@ -1,12 +1,23 @@
 
 // Use 'var' para variaveis que podem mudar o valor
-// use 'val' para variaveis imutaveis
+// Use 'val' para variaveis imutaveis
+// Não é possivel criar uma variavel sem inicializa-la
+//O kotlin permite deixar implicito o tipo de retorno
 
 fun main() {
     println("Bem vindo ao Bytebank!")
-    val titular = "Renato"
+    val titular: String = "Renato"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 200
+
 
     //String template
     println("titular: $titular")
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldo")
+
 
 }
