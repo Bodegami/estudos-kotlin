c01b5babb6e807e51eae5998d2b042ef47ba5f48
Renato <rena.f.oliveira@gmail.com>
2022-12-23 09:37:13 -0300

    sobre estruturas condicionais e o When

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/main.kt
@@ -12,12 +12,33 @@ fun main() {
 
     saldo = 100 + 2.0
     saldo += 200
-
+    saldo = 0.0
+    saldo -= 1000
 
     //String template
     println("titular: $titular")
     println("numero da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    //Utilizando o if, else if e else
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if(saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
+
+    // When expression -> equivalente ao switch case
+    // A sintaxe consiste na chamada do 'when', depois a condição, seguida por '->' que indica o comando a ser executado
+    // As chaves são opcionais quando o comando é somente uma linha de código
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> {
+            println("conta é negativa")
+        }
+    }
 
 
 }
