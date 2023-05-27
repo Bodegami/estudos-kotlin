c67eb5bc35715e46d7360c5d99a4974b1cfdc082
Renato <rena.f.oliveira@gmail.com>
2023-05-27 10:59:22 -0300

    organizando os projetos && inicio curso kotlin-collections-set-e-map

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
@@ -2,32 +2,6 @@ package br.com.bodegami.bytebank
 
 fun main() {
 
-    var salarios: DoubleArray = doubleArrayOf(1500.0, 2300.0, 5000.0, 8000.0, 10000.0)
-
-    val aumento = 1.1
-    var posicao = 0
-    for (salario in salarios) {
-        salarios[posicao] = salario * aumento
-        posicao++
-    }
-    println(salarios.contentToString())
-
-
-    for (indice in salarios.indices) {
-        salarios[indice] = salarios[indice] * aumento
-    }
-    println(salarios.contentToString())
-
-
-    salarios.forEachIndexed { indice, salario ->
-        salarios[indice] = salario * aumento
-    }
-    println(salarios.contentToString())
-
-
-    val salariosComAumento = salarios.map { salario -> salario * aumento }
-    println(salariosComAumento)
-
 }
 
 
