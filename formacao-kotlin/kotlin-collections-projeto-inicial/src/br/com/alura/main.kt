bb86752fc5bf80e70cf9b77cc344471d4a44c68c
Renato <rena.f.oliveira@gmail.com>
2023-05-27 11:07:20 -0300

    introdução a Collections

--- formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -2,4 +2,14 @@ package br.com.alura
 
 fun main() {
 
+    val nomes: Collection<String> = setOf("Alex", "Fran", "Gui", "Maria", "Ana")
+    println(nomes)
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção ${nomes.size}")
+
 }
