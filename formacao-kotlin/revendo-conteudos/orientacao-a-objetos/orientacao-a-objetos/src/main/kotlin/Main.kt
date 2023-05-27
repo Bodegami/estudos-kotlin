ff039ca8ca7ad320993b037915f7a1160625cdfc
Renato <rena.f.oliveira@gmail.com>
2023-05-27 18:14:07 -0300

    criando classes e objetos

--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val contaRenato = Conta()
+    contaRenato.titular = "Renato"
+    contaRenato.numero = 1000
+    contaRenato.saldo = 200.0
+    println(contaRenato.titular)
+    println(contaRenato.numero)
+    println(contaRenato.saldo)
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
 }
