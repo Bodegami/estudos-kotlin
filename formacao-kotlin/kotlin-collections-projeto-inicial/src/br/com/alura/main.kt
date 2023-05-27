805ce19f9550eec88818fb75ec26a98769822a03
Renato <rena.f.oliveira@gmail.com>
2023-05-27 17:03:30 -0300

    Tipos de coleção

--- formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -2,6 +2,30 @@ package br.com.alura
 
 fun main() {
 
+    val banco = BancoDeNomes()
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = Companion.dados
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+
+}
+
+
+private fun testaColecao() {
     val nomes: Collection<String> = setOf("Alex", "Fran", "Gui", "Maria", "Ana")
     println(nomes)
 
@@ -11,5 +35,4 @@ fun main() {
 
     println("Tem o nome Alex? ${nomes.contains("Alex")}")
     println("Tamanho da coleção ${nomes.size}")
-
 }
