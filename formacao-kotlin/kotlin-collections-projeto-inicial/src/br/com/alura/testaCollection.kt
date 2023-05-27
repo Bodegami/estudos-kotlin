39362543fadcaaf5404b28a32d16d79809376780
Renato <rena.f.oliveira@gmail.com>
2023-05-27 17:22:30 -0300

    Devolvendo elementos com distinc

---
+++ formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/testaCollection.kt
@@ -0,0 +1,38 @@
+package br.com.alura
+
+public fun testaCopia() {
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+    banco.salva("Alex")
+    //nomesSalvos.add("Paulo")
+    println(nomesSalvos) // recebeu uma copia da lista
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = Companion.dados.toList()
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+
+}
+
+
+public fun testaColecao() {
+    val nomes: Collection<String> = setOf("Alex", "Fran", "Gui", "Maria", "Ana")
+    println(nomes)
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("Tem o nome Alex? ${nomes.contains("Alex")}")
+    println("Tamanho da coleção ${nomes.size}")
+}
