378c19230247a3f6fda69fff3dcbf91ae2f5a9b0
Renato <rena.f.oliveira@gmail.com>
2023-05-27 23:55:07 -0300

    criando uma nova classe

--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
     println("Hello Byte Bank!")
 
-    testaComportamentosConta()
+
+    val renato = Funcionario(
+        nome = "Renato",
+        cpf = "11122233301",
+        salario = 1000.0
+    )
+
+    println("nome $${renato.nome}")
+    println("cpf $${renato.cpf}")
+    println("salario $${renato.salario}")
+    println("bonificacao ${renato.bonificacao()}")
 
 }
 
