5373c805cb2cebe62f9d7fac6cbf664d6bf39eca
Renato <rena.f.oliveira@gmail.com>
2023-05-31 21:19:02 -0300

    utilizando o Any no codigo

--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -1,34 +1,31 @@
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.teste.testaFuncionarios
 
 fun main(args: Array<String>) {
-
-    println()
-    println(1)
-    println(1.0)
-    println(true)
-    println("teste")
-
     val endereco = Endereco()
 
     println(endereco)
 
+    imprime(Unit)
     imprime(1)
     imprime(2.0)
     imprime(endereco)
 
-}
+    val objeto = Any()
+    println(objeto)
 
-fun imprime(valor: Int) {
+    val teste: Any = imprime(endereco)
+    println(teste)
 
-}
-
-fun imprime(valor: Double) {
-
-}
-
-fun imprime(valor: Endereco) {
+    testaFuncionarios()
 
 }
+
+fun imprime(valor: Any) : Any {
+    println(valor)
+    return valor
+}
+
 
 
 
--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
@@ -1,7 +1,5 @@
 package br.com.alura.bytebank.modelo
 
-import br.com.alura.bytebank.modelo.Funcionario
-
 class CalculadoraBonificacao {
 
     var total: Double = 0.0
@@ -11,5 +9,15 @@ class CalculadoraBonificacao {
         println("nome ${funcionario.nome}")
         this.total += funcionario.bonificacao()
     }
+
+    /**
+     *
+     *
+     *         funcionario as Funcionario -> faz o cast e altera a mesma variavel, caso contrario lanca exception
+     *         if (funcionario is Funcionario) { // -> verifica e faz o smart cast e altera a mesma variavel
+     *             this.total += funcionario.bonificacao()
+     *         }
+     *
+     */
 
 }
