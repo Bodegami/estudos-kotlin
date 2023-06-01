f17b1495e9a1ca0a198906832d87d96f3f3ac6d6
Renato <rena.f.oliveira@gmail.com>
2023-06-01 20:52:08 -0300

    Utilizando o safe call

--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -2,15 +2,27 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    // Por padrao os tipos não podem receber nulo, para isso precisamos indicar com o interrogação
+    // Por padrao os tipos não podem receber nulo (nullable), para isso precisamos indicar com o interrogação
     //quando um tipo pode receber nulo. Ex: Endereco?
     var enderecoNulo: Endereco? = null
 
-    // Podemos também indicar ao kotlin que temos certeza que um objeto não é nulo, para isso
+    // Podemos também indicar ao kotlin que temos certeza que um objeto não é nulo (non-nullable), para isso
     //indicamos com !!. Ex: endereco!!
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
+    //val enderecoNaoNulo: Endereco = enderecoNulo!!
+    //enderecoNaoNulo.logradouro
 
-    enderecoNaoNulo.logradouro
+
+    var enderecoNovo: Endereco? = Endereco(logradouro = "Vergueiro")
+    val logradouroNovo: String? = enderecoNovo?.logradouro
+    println(logradouroNovo?.length?.toUByte())
+
+    //Com o .let{} (scope function) podemos apenas passar o safe call operator antes do let, de forma que
+    //qualquer atributo que tentarmos acessar dentro do bloco, não somos obrigados a passar o safe call
+    enderecoNovo?.let {
+        println(it.logradouro)
+        println(it.cep.length)
+
+    }
 
 }
 
