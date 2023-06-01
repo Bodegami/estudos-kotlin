9399040b3cb36b54b209af8a336e1209916c02ed
Renato <rena.f.oliveira@gmail.com>
2023-05-31 21:43:28 -0300

    Sobrescrevendo o equals e o hashcode && conclusao revisao-recursos-da-linguagem-com-pacotes-e-composicao

--- formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
+++ formacao-kotlin/revendo-conteudos/orientacao-a-objetos/orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main(args: Array<String>) {
 
-    val endereco = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura")
-    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+    val endereco = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura", cep = "00000-000")
+    val enderecoNovo = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura", cep = "00000-000")
 
     println(endereco.equals(enderecoNovo))
 
@@ -16,6 +16,8 @@ fun main(args: Array<String>) {
 
     println("${endereco.javaClass}@${Integer.toHexString(endereco.hashCode())}")
 
+
+    println(endereco.equals(enderecoNovo))
 
 }
 
