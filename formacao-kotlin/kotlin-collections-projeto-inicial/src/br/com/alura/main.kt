320dc43e43b20e682cb9318182f1c50e46086652
Renato <rena.f.oliveira@gmail.com>
2023-06-03 12:27:42 -0300

    Leitura e filtros especificos de Map

--- formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ formacao-kotlin/kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -1,11 +1,70 @@
 package br.com.alura
 
+import java.security.Key
+
 fun main() {
 
     // o Infix "to" facilita a leitura, mas para estruturas muitos grandes ele apresenta uma perda de desempenho
-    val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(Pair(1, 20.0), Pair(2, 23.0), 2 to 50.0, 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 64.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 150.0,
+        6 to 80.0
+    )
     println(pedidos)
 
+    //Retorna nulo caso não encontre a chave no mapa
+    var valorPedido: Double? = pedidos[0]
+    println(valorPedido)
+
+    //Retorna nulo caso não encontre a chave no mapa
+    valorPedido = pedidos.get(0)
+    println(valorPedido)
+
+    //Caso nao encontre a chave no mapa, lança uma NoSuchElementException
+    val valorPedidoComGetValue = try { pedidos.getValue(4) } catch (e: NoSuchElementException) { "Exception: ${e.javaClass.simpleName} | Message: ${e.message}" }
+    println(valorPedidoComGetValue)
+
+    //Caso não encontre a chave no mapa, executa o código no bloco passado como argumento
+    println(pedidos.getOrElse( 5) { "Não tem o pedido" })
+
+    //Caso não encontre a chave no ampa, retorna um valor padrão
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+
+    for ((chave, valor) in pedidos) {
+        println("Valor do pedido $chave: $valor ")
+    }
+
+    for (valor in pedidos.keys) {
+        println("Valor do pedido: $valor ")
+    }
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 50.0
+    }
+    println("Pedidos Filtrados: $pedidosFiltrados")
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+    println("Pedidos Acima: $pedidosAcima")
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+    println("Pedidos Pares: $pedidosPares")
+
+    val mapaFiltrado: MutableMap<Int, Double> = mutableMapOf<Int, Double>()
+    pedidos.filterTo(mapaFiltrado) { it.key % 2 == 0 && it.value > 70.0 }.run { println("Mapa Filtrado: $this") }
+
+
+}
+
+fun testaMap(pedidos: MutableMap<Int, Double>) {
     val pedido = pedidos[1]
     pedido?.let {
         println("pedido: $it")
@@ -39,7 +98,4 @@ fun main() {
     //sobrecarga do metodos remove que só elimina o elemento caso o valor seja igual
     pedidos.remove(3, 100.0)
     println(pedidos)
-
-
-
 }
