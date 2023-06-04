e6c33e5087d284f36dacdbc25385e1a51413f695
Renato <rena.f.oliveira@gmail.com>
2023-06-03 23:48:31 -0300

    Tipo função com parâmetros e retorno

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
@@ -2,50 +2,56 @@ package br.com.bodegami.bytebank
 
 fun main() {
 
-    /**
-     * Assim como no Python, as funções são Firt-Class, ou seja,
-     * elas podem ser assinadas como variaveis ou passadas como
-     * parametros.
-     */
+//    /**
+//     * Assim como no Python, as funções são Firt-Class, ou seja,
+//     * elas podem ser assinadas como variaveis ou passadas como
+//     * parametros.
+//     */
 
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+
+
+//    testaFuncaoLambda()
+//
+//    testaTipoFuncaoAnonima()
+
+
+}
+
+fun testaFuncaoLambda() {
     //inicializacao atraves de uma expressão lambda
     val minhaFuncaoLambda = {
         println("Executa como Lambda!")
     }
     println(minhaFuncaoLambda())
+}
 
+fun testaTipoFuncaoAnonima() {
     //inicializacao atraves de uma funcao anonima
     val minhaFuncaoAnonima: () -> Unit = fun() {
         println("Executa como funcao anonima!")
     }
     println(minhaFuncaoAnonima())
-
-
-
 }
 
 fun testaTipoFuncaoReferencia() {
     //inicializacao atraves da referencia de funcao
-
-    val minhaFuncao = ::teste
-    println(minhaFuncao())
+    //devemos indicar os paramtros de entra e saida. ex: (Int, Int) -> Int
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 10))
 }
 
 fun testaTipoFuncaoClasse() {
     //inicializacao atraves de um objeto de classe com a reimplementacao do invoke
 
-    val minhaFuncaoClasses: () -> Unit = Teste()
-    println(minhaFuncaoClasses())
+    val minhaFuncaoClasses: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClasses(10, 10))
 }
 
-fun teste() {
-    println("executa teste")
-}
+fun soma(a: Int, b: Int) : Int = a + b
 
-class Teste: () -> Unit {
-    override fun invoke() {
-        println("executa invoke do Teste")
-    }
-
+class Soma: (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int = p1 + p2
 
 }
