1fd7e298f830481318f17fa52977b7ce5d70da0e
Renato <rena.f.oliveira@gmail.com>
2023-06-04 20:50:01 -0300

    Implementando Higher Order Functions

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
@@ -1,6 +1,8 @@
 package br.com.bodegami.bytebank
 
+import br.com.bodegami.bytebank.modelo.Autenticavel
 import br.com.bodegami.bytebank.modelo.Endereco
+import br.com.bodegami.bytebank.modelo.SistemaInterno
 
 fun main() {
 
@@ -23,4 +25,23 @@ fun main() {
             println(enderecosComComplemento.size)
         }
 
+    soma(1, 5, resultado = {
+        println(it)
+    })
+
+    val autenticavel = object: Autenticavel {
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("realizar pagamento")
+    })
+
+}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    println("antes da soma")
+    resultado(a + b)
+    println("depois da soma")
 }
--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/modelo/SistemaInterno.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/modelo/SistemaInterno.kt
@@ -4,9 +4,10 @@ import br.com.bodegami.bytebank.modelo.Autenticavel
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: Int) {
+    fun entra(admin: Autenticavel, senha: Int, autenticado: () -> Unit = {}) {
         if (admin.autentica(senha)) {
             println("Bem vindo ao ByteBank!")
+            autenticado()
         }
         else {
             println("Falha na autenticação!")
