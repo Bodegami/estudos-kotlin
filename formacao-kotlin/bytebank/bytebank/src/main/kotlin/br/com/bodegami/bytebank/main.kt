e0cf1c3bfbc676583124be25ec8c056b23a98073
Renato <rena.f.oliveira@gmail.com>
2023-06-04 22:19:26 -0300

    Testa o run

--- formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
+++ formacao-kotlin/bytebank/bytebank/src/main/kotlin/br/com/bodegami/bytebank/main.kt
@@ -1,12 +1,40 @@
 package br.com.bodegami.bytebank
 
+import br.com.bodegami.bytebank.modelo.Cliente
+import br.com.bodegami.bytebank.modelo.ContaPoupanca
 import br.com.bodegami.bytebank.modelo.Endereco
 
 fun main() {
 
     //testaWith()
+    //testaRun()
 
 
+
+
+}
+
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("taxa mensal $taxaMensal")
+
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "Alex", cpf = "111.222.333-01", senha = 1234), 1000)
+    contaPoupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println("rendimento mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) { indice ->
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Simulacao rendimento anual: $rendimentoAnual")
 }
 
 fun testaWith() {
