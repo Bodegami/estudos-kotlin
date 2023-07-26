5da8e6c3704fe0a975b20d7b3019449588e53807
Renato <rena.f.oliveira@gmail.com>
2023-07-25 22:10:12 -0300

    Cadastrando um topico

--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -4,6 +4,8 @@ import br.com.alura.forum.model.Topico
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -20,6 +22,11 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico {
         return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico) {
+        service.cadastrar(topico)
     }
 
 }
--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,66 +1,11 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
-import java.util.*
+import kotlin.collections.ArrayList
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
-
-    init {
-
-        val topico1 = Topico(
-            id = 1,
-            titulo = "Duvida Kotlin 1",
-            mensagem = "Variaveis no Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        val topico2 = Topico(
-            id = 2,
-            titulo = "Duvida Kotlin 2",
-            mensagem = "Variaveis no Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        val topico3 = Topico(
-            id = 3,
-            titulo = "Duvida Kotlin 3",
-            mensagem = "Variaveis no Kotlin",
-            curso = Curso(
-                id = 1,
-                nome = "Kotlin",
-                categoria = "Programação"
-            ),
-            autor = Usuario(
-                id = 1,
-                nome = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        topicos = Arrays.asList(topico1, topico2, topico3)
-    }
+class TopicoService(private var topicos: List<Topico> = ArrayList<Topico>()) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -70,5 +15,9 @@ class TopicoService(private var topicos: List<Topico>) {
         return topicos.stream().filter { topico ->
             topico.id == id
         }.findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 }
