3958ffb0a3f21ba41367e3331460a2bb0343aefe
Renato <rena.f.oliveira@gmail.com>
2023-07-27 20:42:41 -0300

    DTOs de entrada e de saida

--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,7 +1,7 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.dto.NovoTopicoDto
-import br.com.alura.forum.model.Topico
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,17 +16,17 @@ import java.util.*
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    fun listar(): List<Topico> {
+    fun listar(): List<TopicoView> {
         return service.listar()
     }
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id: Long): Topico {
+    fun buscarPorId(@PathVariable id: Long): TopicoView {
         return service.buscarPorId(id)
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoDto) {
+    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
         service.cadastrar(dto)
     }
 
--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,6 +1,7 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
 import kotlin.collections.ArrayList
@@ -12,24 +13,37 @@ class TopicoService(
     private val usuarioService: UsuarioService
 ) {
 
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map { topico -> TopicoView(
+            id = topico.id,
+            titulo = topico.titulo,
+            mensagem = topico.mensagem,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        ) }.toList()
     }
 
-    fun buscarPorId(id: Long): Topico {
+    fun buscarPorId(id: Long): TopicoView {
         return topicos.stream().filter { topico ->
             topico.id == id
-        }.findFirst().get()
+        }.findFirst().get().let { topico ->
+            TopicoView(
+                id = topico.id,
+                titulo = topico.titulo,
+                mensagem = topico.mensagem,
+                status = topico.status,
+                dataCriacao = topico.dataCriacao
+            )
+        }
     }
 
-    fun cadastrar(dto: NovoTopicoDto) {
+    fun cadastrar(dto: NovoTopicoForm) {
         topicos = topicos.plus(Topico(
             id = topicos.size.toLong() + 1,
             titulo = dto.titulo,
             mensagem = dto.mensagem,
             curso = cursoService.buscarPorId(id = dto.idCurso),
             autor = usuarioService.buscarPorId(id = dto.idAutor),
-
         ))
     }
 }
