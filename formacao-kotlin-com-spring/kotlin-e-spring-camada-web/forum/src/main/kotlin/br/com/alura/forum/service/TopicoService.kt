161030241caa3486cac701b709ce657770ad7e96
Renato <rena.f.oliveira@gmail.com>
2023-07-27 21:50:44 -0300

    Utilizando mappers para conversão de DTOs

--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -2,6 +2,8 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
 import kotlin.collections.ArrayList
@@ -9,41 +11,27 @@ import kotlin.collections.ArrayList
 @Service
 class TopicoService(
     private var topicos: List<Topico> = ArrayList<Topico>(),
-    private val cursoService: CursoService,
-    private val usuarioService: UsuarioService
+    private val topicoVierMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper
 ) {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map { topico -> TopicoView(
-            id = topico.id,
-            titulo = topico.titulo,
-            mensagem = topico.mensagem,
-            status = topico.status,
-            dataCriacao = topico.dataCriacao
-        ) }.toList()
+        return topicos.stream().map { topico ->
+            topicoVierMapper.map(topico)
+        }.toList()
     }
 
     fun buscarPorId(id: Long): TopicoView {
-        return topicos.stream().filter { topico ->
-            topico.id == id
-        }.findFirst().get().let { topico ->
-            TopicoView(
-                id = topico.id,
-                titulo = topico.titulo,
-                mensagem = topico.mensagem,
-                status = topico.status,
-                dataCriacao = topico.dataCriacao
-            )
-        }
+        return topicos.stream()
+            .filter { topico -> topico.id == id }
+            .findFirst()
+            .get()
+            .let{ topico -> topicoVierMapper.map(topico) }
     }
 
-    fun cadastrar(dto: NovoTopicoForm) {
-        topicos = topicos.plus(Topico(
-            id = topicos.size.toLong() + 1,
-            titulo = dto.titulo,
-            mensagem = dto.mensagem,
-            curso = cursoService.buscarPorId(id = dto.idCurso),
-            autor = usuarioService.buscarPorId(id = dto.idAutor),
-        ))
+    fun cadastrar(form: NovoTopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
     }
 }
