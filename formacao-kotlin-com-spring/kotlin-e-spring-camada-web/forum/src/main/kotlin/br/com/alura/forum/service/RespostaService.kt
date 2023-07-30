52035bb96c0596d46e3bb08b3d40b9f042e3b938
Renato <rena.f.oliveira@gmail.com>
2023-07-29 21:59:07 -0300

    Update e Delete Resposta

--- formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/RespostaService.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-web/forum/src/main/kotlin/br/com/alura/forum/service/RespostaService.kt
@@ -1,7 +1,10 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoRespostaForm
 import br.com.alura.forum.dto.NovaRespostaForm
+import br.com.alura.forum.dto.RespostaView
 import br.com.alura.forum.mapper.RespostaFormMapper
+import br.com.alura.forum.mapper.RespostaViewMapper
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Resposta
 import br.com.alura.forum.model.Topico
@@ -14,7 +17,8 @@ import java.util.stream.Collectors
 class RespostaService(
     private var respostas: List<Resposta>,
     private val topicoService: TopicoService,
-    private val respostaFormMapper: RespostaFormMapper
+    private val respostaFormMapper: RespostaFormMapper,
+    private val respostaViewMapper: RespostaViewMapper
 ) {
 
     init {
@@ -66,5 +70,27 @@ class RespostaService(
         resposta.id = respostas.size.toLong() + 1
         resposta.topico = topicoService.buscarPorIdTopico(idTopico)
         respostas = respostas.plus(resposta)
+    }
+
+    fun atualizar(form: AtualizacaoRespostaForm): RespostaView {
+        val resposta = respostas.stream().filter { r ->
+            r.id == form.id
+        }.findFirst().get()
+        val respostaAtualizada = Resposta(
+            id = form.id,
+            mensagem = form.mensagem,
+            autor = resposta.autor,
+            topico = resposta.topico,
+            dataCriacao = resposta.dataCriacao
+        )
+        respostas = respostas.minus(resposta).plus(respostaAtualizada)
+        return respostaViewMapper.map(respostaAtualizada)
+    }
+
+    fun deletar(id: Long) {
+        val resposta = respostas.stream().filter { r ->
+            r.id == id
+        }.findFirst().get()
+        respostas = respostas.minus(resposta)
     }
 }
