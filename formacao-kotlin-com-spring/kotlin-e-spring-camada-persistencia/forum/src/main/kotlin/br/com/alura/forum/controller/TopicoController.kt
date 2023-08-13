ba7eadaec33b85e9a5cd8efba2b649ae9a70b70d
Renato <rena.f.oliveira@gmail.com>
2023-08-12 23:12:47 -0300

    ordenacao dos dados

--- formacao-kotlin-com-spring/kotlin-e-spring-camada-persistencia/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ formacao-kotlin-com-spring/kotlin-e-spring-camada-persistencia/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -6,6 +6,7 @@ import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -31,8 +32,10 @@ class TopicoController(private val service: TopicoService) {
     @GetMapping
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
-        @PageableDefault(size = 4) paginacao: Pageable
+        @PageableDefault(size = 4, sort = ["dataCriacao"],
+            direction = Sort.Direction.DESC) paginacao: Pageable
     ): Page<TopicoView> {
+
         return service.listar(nomeCurso, paginacao)
     }
 
