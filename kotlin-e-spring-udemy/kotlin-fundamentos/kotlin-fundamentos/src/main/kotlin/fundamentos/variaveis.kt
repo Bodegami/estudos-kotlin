9c7096f681c06cb5dff510147f8142634636b705
Renato <rena.f.oliveira@gmail.com>
2023-08-18 19:52:34 -0300

    variaveis

---
+++ kotlin-e-spring-udemy/kotlin-fundamentos/kotlin-fundamentos/src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,28 @@
+package fundamentos
+
+fun main() {
+    var nome = "renato"
+    val nomeVal = "Renato"
+
+    nome = "Renato F."
+    //nomeVal = "Renato F"
+
+    val idade = 24
+    println(idade)
+
+    val teste: String
+    teste = "Teste"
+
+    var texto = "Texto"
+    //texto = 123
+    
+}
+
+
+class variaveis {
+    lateinit var  teste: String
+
+    fun iniciaVariaveis() {
+        teste = "Teste"
+    }
+}
