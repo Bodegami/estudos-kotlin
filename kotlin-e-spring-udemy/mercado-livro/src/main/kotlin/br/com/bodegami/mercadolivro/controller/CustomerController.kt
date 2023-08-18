1ad13f34916a4143330cbf67eac606639fa50fd5
Renato <rena.f.oliveira@gmail.com>
2023-08-17 22:24:03 -0300

    adicionando query params

--- kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/CustomerController.kt
+++ kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/CustomerController.kt
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,7 +22,10 @@ class CustomerController {
     val custormers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun findAll(): List<CustomerModel> {
+    fun findAll(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return custormers.filter { it.name.contains(name, true) }
+        }
         return custormers
     }
 
