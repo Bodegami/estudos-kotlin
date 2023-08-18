b272b24a76262aa3e66230c05d25d3e85555193e
Renato <rena.f.oliveira@gmail.com>
2023-08-17 22:16:48 -0300

    Implementando o update customer

--- kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/CustomerController.kt
+++ kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/CustomerController.kt
@@ -1,11 +1,13 @@
 package br.com.bodegami.mercadolivro.controller
 
 import br.com.bodegami.mercadolivro.controller.request.PostCustomerRequest
+import br.com.bodegami.mercadolivro.controller.request.PutCustomerRequest
 import br.com.bodegami.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -18,12 +20,12 @@ class CustomerController {
     val custormers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getAll(): List<CustomerModel> {
+    fun findAll(): List<CustomerModel> {
         return custormers
     }
 
     @GetMapping("/{id}")
-    fun getCustomer(@PathVariable id: String): CustomerModel {
+    fun findById(@PathVariable id: String): CustomerModel {
         return custormers.first { it ->
             it.id == id
         }
@@ -34,6 +36,17 @@ class CustomerController {
     fun create(@RequestBody customer: PostCustomerRequest) {
         custormers.add(CustomerModel(getId(), customer.name, customer.email))
         print(custormers)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+        custormers.first { it ->
+            it.id == id
+        }.let { it ->
+            it.name = customer.name
+            it.email = customer.email
+        }
     }
 
 
