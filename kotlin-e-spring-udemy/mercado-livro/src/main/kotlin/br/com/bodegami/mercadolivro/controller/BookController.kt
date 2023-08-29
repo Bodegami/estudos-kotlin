20d809d663bb21fa8d03b4b037323d507f044555
Renato <rena.f.oliveira@gmail.com>
2023-08-28 22:21:37 -0300

    criando as classes de error

--- kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/BookController.kt
+++ kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/BookController.kt
@@ -63,5 +63,4 @@ class BookController(
         bookService.delete(id)
     }
 
-
 }
---
+++ kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/controller/response/FieldErrorResponse.kt
@@ -0,0 +1,6 @@
+package br.com.bodegami.mercadolivro.controller.response
+
+data class FieldErrorResponse(
+    var field: String,
+    var message: String
+)
