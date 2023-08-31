036c422add4f3f00fbe8b323498df486ce2b973c
Renato <rena.f.oliveira@gmail.com>
2023-08-31 00:01:02 -0300

    Tratando a exception lancada

--- kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/exception/ControllerAdvice.kt
+++ kotlin-e-spring-udemy/mercado-livro/src/main/kotlin/br/com/bodegami/mercadolivro/exception/ControllerAdvice.kt
@@ -1,8 +1,11 @@
 package br.com.bodegami.mercadolivro.exception
 
 import br.com.bodegami.mercadolivro.controller.response.ErrorResponse
+import br.com.bodegami.mercadolivro.controller.response.FieldErrorResponse
+import br.com.bodegami.mercadolivro.enums.Errors
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -32,6 +35,20 @@ class ControllerAdvice {
         )
 
         return ResponseEntity(erro, HttpStatus.BAD_REQUEST)
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException,
+                                              request: WebRequest): ResponseEntity<ErrorResponse> {
+        val erro = ErrorResponse(
+            HttpStatus.UNPROCESSABLE_ENTITY.value(),
+            Errors.ML001.message,
+            Errors.ML001.code,
+            ex.fieldErrors.map { FieldErrorResponse(it.field, it.defaultMessage ?: "invalid") }.toList()
+        )
+
+        return ResponseEntity(erro, HttpStatus.UNPROCESSABLE_ENTITY)
     }
 
 
