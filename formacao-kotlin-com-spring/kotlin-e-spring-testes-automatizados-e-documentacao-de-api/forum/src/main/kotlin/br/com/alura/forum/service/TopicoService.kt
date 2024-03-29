3686337a125635615b9c0f3e73c0b36f3df11dfc
Renato <rena.f.oliveira@gmail.com>
2024-03-29 20:30:35 -0300

    inicio curso kotlin-e-spring-testes-automatizados-e-documentacao-de-api && setup inicial

---
+++ formacao-kotlin-com-spring/kotlin-e-spring-testes-automatizados-e-documentacao-de-api/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,67 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoPorCategoriaDto
+import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.exception.NotFoundException
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
+import br.com.alura.forum.repository.TopicoRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import javax.persistence.EntityManager
+
+@Service
+class TopicoService(
+    private val repository: TopicoRepository,
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper,
+    private val notFoundMessage: String = "Topico nao encontrado",
+    private val em: EntityManager
+) {
+
+    fun listar(
+        nomeCurso: String?,
+        paginacao: Pageable
+        ): Page<TopicoView> {
+        print(em)
+        val topicos = if (nomeCurso == null) {
+            repository.findAll(paginacao)
+        } else {
+            repository.findByCursoNome(nomeCurso, paginacao)
+        }
+
+        return topicos.map(topicoViewMapper::map)
+    }
+
+    fun buscarPorId(id: Long): TopicoView {
+        return repository.findById(id)
+            .orElseThrow{ NotFoundException(notFoundMessage) }
+            .let{ topico -> topicoViewMapper.map(topico) }
+    }
+
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
+        val topico = topicoFormMapper.map(form)
+        repository.save(topico)
+        return topicoViewMapper.map(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
+        val topico = repository.findById(form.id)
+            .orElseThrow{ NotFoundException(notFoundMessage) }
+
+        topico.titulo = form.titulo
+        topico.mensagem = form.mensagem
+        return topicoViewMapper.map(topico)
+    }
+
+    fun deletar(id: Long) {
+        repository.deleteById(id)
+    }
+
+    fun relatorio(): List<TopicoPorCategoriaDto> {
+        return repository.relatorio()
+    }
+}
